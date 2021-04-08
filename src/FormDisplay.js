0dd7817c15afd6babd7f3b05b9f5996b0ae52a88
James Marriott <sawheadproductions@gmail.com>
2021-04-08 10:38:33 +0200

    first version

--- src/FormDisplay.js
+++ src/FormDisplay.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import { Card, Col, Row, Container, Form, InputGroup, NavItem } from "react-bootstrap"
+import { Jumbotron, Card, Col, Row, Container, Form, InputGroup, NavItem } from "react-bootstrap"
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -16,15 +16,14 @@ const FormDisplay = ({ madLibTitle, imageURL, madLibText, subWords,  ...other })
     const [isMadDisplayed, setIsMadDisplayed] = useState(false)
 
     const toggleMad = () => setIsMadDisplayed(!isMadDisplayed)
-    
+        
     function handleSubmit(event) {
       event.preventDefault()
       setSubmitted(prevArray =>
         [... prevArray,
-          //here is a problem
+          // form validation setup
         ])
     }
-    // error checking - input field max length
   
     function handleChange(event, sub) {
       const {name, value} = event.target
@@ -34,27 +33,36 @@ const FormDisplay = ({ madLibTitle, imageURL, madLibText, subWords,  ...other })
         )
       setMadTextDisplay(prevDisplay => {
         const newDisplay = [...prevDisplay]
-        newDisplay[0][sub.InpPos-1] = value
+        newDisplay[0][sub.InpPos-1] = value + sub.TrailingPunct
         return newDisplay
       })
+      console.log(madTextDisplay)
       }
 
+
 return (
-        <Container>
-            <h1 className="text-center">{madLibTitle}</h1>
-            <div className="text-center">
+        <Container className="text-center">
+            <Card.Header>
+            <h1>MadLibz!</h1>
+            {/* <h5>A <Card.Link href="https://en.wikipedia.org/wiki/Mad_Libs">MadLibz app</Card.Link></h5> */}
+            <p>Enter words - adjectives, nouns etc. - using the form below and click 'Create' to make a funny story.</p>
+            </Card.Header>
             <br/>
-              <Button variant="success" onClick={toggleMad}>{isMadDisplayed ? `Hide Mad!` : `Get Mad!`}</Button>
-            </div>
+            <Card.Header>
+            <h2 style={{fontStyle: 'italic'}}>{madLibTitle}</h2>
             <br/>
+            <Button variant="primary" onClick={toggleMad}>{isMadDisplayed ? `Reset!` : `Create`}</Button>
+            <br/>
+            </Card.Header>
 
           {!isMadDisplayed ? <div>
             <br />
-                <Form className='align-items-center'>
+                <Form>
                 <Form.Row className='align-items-center'>
                     {Object.values(subWords).map(sub=>
                         <Col xs="auto">
-                      <Form.Control
+                      <Form.Control 
+                      size="lg"
                       key="sub.SubId"
                       type="text"
                       name={sub.SubId}
@@ -65,32 +73,27 @@ return (
                       </Col>
                     )}
                 </Form.Row>
-                  {/* <div className="text-center">
-                      <Button type="submit">Get Mad!</Button>
-                    </div> */}
                 </Form>
                 </div>
           : null}
           {isMadDisplayed ?
-            <Card className="bg-dark text-white">
-            <Card.Body>
-              <Card.Img src={imageURL}/>
+            <Card className="bg-dark text-white custom-card">
+            <Card.Body style={{fontSize: 'max(1.4vw, 14px)'}}>
+              <Card.Img className="op-img" src={imageURL} alt="Image behind text"/>
               <Card.ImgOverlay>
-              {/* <Card.Title>{madLibTitle}</Card.Title> */}
               <Card.Text>
               {madTextDisplay[0].map((word, Id) =>
-              <span style={{fontSize : '32px'}} key={Id}> {word}
+              <span key={Id}> {word}
               </span>
               )}
               </Card.Text>
               </Card.ImgOverlay>
             </Card.Body>
           </Card> : null }
+          <br/>
+          <Card.Footer className="text-muted text-center">App developed with React Hooks. More features and MadLibz coming soon! Help develop the API.  Contact: <Card.Link href="https://github.com/jamesmarriott/">James Marriott</Card.Link> </Card.Footer>
       </Container>
     )
 }
 
 export default FormDisplay
-
-
-
